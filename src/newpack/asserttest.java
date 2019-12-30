34cc467cd604eeef4ccbed4e0b69822db07a897f
Santhosh Thekke Neetiath <U35035@ust-global.com>
2019-12-30 10:29:19 +0530

    new class file added

---
+++ src/newpack/asserttest.java
@@ -0,0 +1,40 @@
+package newpack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
+public class asserttest {
+	
+	public static void main (String[] args) throws Exception{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\U35035\\eclipse-workspace\\chromedriver_win32\\chromedriver.exe");
+		
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("http://google.com"); 
+		
+		System.out.println(driver.getTitle());
+		
+		try {
+			
+			Assert.assertEquals("oogle", driver.getTitle());
+			
+		}
+		
+		catch (Exception e) {
+			System.out.println(e.getStackTrace());
+			Assert.assertEquals("Google", driver.getTitle());
+		}
+			
+		finally {
+			driver.close();
+		}
+		
+	}
+
+}
