9d6b74710da95bcb2db80d39c49fe1cd89526a2e
Santhosh Thekke Neetiath <U35035@ust-global.com>
2020-01-07 15:48:39 +0530

    link check files committed

---
+++ src/newpack/myCIDLinksCheck.java
@@ -0,0 +1,142 @@
+package newpack;
+
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import lib.ExcelDataConfig;
+import lib.utility;
+import pagefactory.myRBCID;
+import pagefactory.myRBcommon;
+import pagefactory.myRBlogin;
+
+public class myCIDLinksCheck {
+  @Test
+  public void myCIDLinksCheckViewResults() throws Exception {
+	  
+	  try {
+
+			WebDriver driver;
+
+			String driverpath = "C:\\Users\\U35035\\eclipse-workspace\\chromedriver_win32\\chromedriver.exe";
+
+			String datapath = "C:\\Users\\U35035\\eclipse-workspace\\Data\\TestData.xlsx";
+
+			ExcelDataConfig excel = new ExcelDataConfig(datapath);
+
+			Random rand = new Random();
+
+			// create chrome instance
+			System.setProperty("webdriver.chrome.driver", driverpath);
+
+			for (int i = 0; i <= excel.getrownum(6); i++) {
+
+				driver = new ChromeDriver();
+
+				myRBlogin rb = new myRBlogin(driver);
+				
+					
+				myRBCID rbcid = new myRBCID(driver);
+
+
+				driver.manage().window().maximize();
+
+				// base url
+
+				String baseurl = "https://qa.cid.ie";
+
+				driver.get(baseurl);
+
+				// get webelements
+
+				// WebElement name=driver.findElement(By.id("userName"));
+
+				// WebElement pass=driver.findElement(By.id("password"));
+
+				// pass credential
+
+				rb.setusername(excel.getData(6, i, 0));
+
+				rb.setpassword(excel.getData(6, i, 1));
+				
+				Thread.sleep(5000);
+
+				// RESI value status of user
+
+				// System.out.println("RESI value status is:" + utility.sqlconnector());
+
+				// click submit button
+
+				// driver.findElement(By.className("submit")).click();;
+
+				rb.clickCIDlogin();
+
+				// passed values
+
+				System.out.println("Values passed");
+
+				// Click CID Home Link
+				
+				rbcid.clickCIDHomeLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "CID Home");
+				
+				// Click CID About Us Link
+				
+				rbcid.clickCIDAboutUsLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "CID About Us");
+				
+				// Click CID Subscribe Link
+				
+				rbcid.clickCIDSubscribeLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "CID Subscribe");
+				
+				// Click CID Costs Link
+				
+				rbcid.clickCIDCostsLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "CID Costs");
+				
+				// Click CID Support Link
+				
+				rbcid.clickCIDSupportLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "CID Support");
+				
+				// Click CID Contact Us Link
+				
+				rbcid.clickCIDContactUsLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "CID Contact Us");
+				
+				
+				// close chrome
+				driver.quit();
+
+				System.out.println("Browser closed");
+
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  
+  }
+}
---
+++ src/newpack/myLawlinkLinksCheck.java
@@ -0,0 +1,141 @@
+package newpack;
+
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import lib.ExcelDataConfig;
+import lib.utility;
+import pagefactory.myRBCID;
+import pagefactory.myRBLawlink;
+import pagefactory.myRBlogin;
+
+public class myLawlinkLinksCheck {
+  @Test
+  public void myLawlinkLinksCheckViewResults() {
+	  
+	  try {
+
+			WebDriver driver;
+
+			String driverpath = "C:\\Users\\U35035\\eclipse-workspace\\chromedriver_win32\\chromedriver.exe";
+
+			String datapath = "C:\\Users\\U35035\\eclipse-workspace\\Data\\TestData.xlsx";
+
+			ExcelDataConfig excel = new ExcelDataConfig(datapath);
+
+			Random rand = new Random();
+
+			// create chrome instance
+			System.setProperty("webdriver.chrome.driver", driverpath);
+
+			for (int i = 0; i <= excel.getrownum(7); i++) {
+
+				driver = new ChromeDriver();
+
+				myRBlogin rb = new myRBlogin(driver);
+				
+					
+				myRBLawlink rblawlink = new myRBLawlink(driver);
+
+				driver.manage().window().maximize();
+
+				// base url
+
+				String baseurl = "https://qa.lawlink.ie";
+
+				driver.get(baseurl);
+				
+				rblawlink.clickLawlinkLoginNowlink();
+
+				// get webelements
+
+				// WebElement name=driver.findElement(By.id("userName"));
+
+				// WebElement pass=driver.findElement(By.id("password"));
+
+				// pass credential
+
+				rb.setusername(excel.getData(7, i, 0));
+
+				rb.setpassword(excel.getData(7, i, 1));
+				
+				Thread.sleep(5000);
+
+				// RESI value status of user
+
+				// System.out.println("RESI value status is:" + utility.sqlconnector());
+
+				// click submit button
+
+				// driver.findElement(By.className("submit")).click();;
+
+				rb.clicklawlinklogin();
+
+				// passed values
+
+				System.out.println("Values passed");
+
+				// Click Lawlink Home Link
+				
+				rblawlink.clickLawlinkHomeLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "Lawlink Home");
+				
+				// Click Lawlink About Us Link
+				
+				rblawlink.clickLawlinkAboutUsLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "Lawlink About Us");
+				
+				// Click Lawlink Subscribe Link
+				
+				rblawlink.clickLawlinkSubscribeLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "Lawlink Subscribe");
+				
+				// Click Lawlink Costs Link
+				
+				rblawlink.clickLawlinkCostsLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "Lawlink Costs");
+				
+				// Click Lawlink Support Link
+				
+				rblawlink.clickLawlinkSupportLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "Lawlink Support");
+				
+				// Click Lawlink Contact Us Link
+				
+				rblawlink.clickLawlinkContactUsLink();
+				
+				Thread.sleep(5000);
+				
+				utility.fullscreenshotcapture(driver, "Lawlink Contact Us");
+				
+				
+				// close chrome
+				driver.quit();
+
+				System.out.println("Browser closed");
+
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+  }
+}
