03a16c2a1d36f9449027bfb4e0733e9cadbe265f
Santhosh Thekke Neetiath <santhosh.tn@gmail.com>
2020-05-28 19:28:50 +0530

    committed files

--- testngBatchFile.bat
+++ testngBatchFile.bat
@@ -1,4 +1,4 @@
-set projectLocation=C:\Users\U35035\eclipse-workspace\myrbtest
+set projectLocation=D:\Rochford Brady TestNG
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
